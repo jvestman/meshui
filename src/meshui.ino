6132786ae98e94ca8c3d37587b9bc96ce825a5ae
Jussi Vestman <jussi.vestman@gmail.com>
2016-07-22 22:28:42 +0300

    Start API

--- src/meshui.ino
+++ src/meshui.ino
@@ -30,16 +30,10 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_AP);
+  WiFI.softAP(ssid, password);
+  //WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  // Start the server
   server.begin();
   Serial.println("Server started");
 
